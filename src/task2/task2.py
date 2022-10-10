dea77cfeb3af04ab8021a1a4fc4da7c245c3a092
Usman Ahmad <uah5512@gmail.com>
2022-10-10 17:54:57 +0300

    Unfinished scaffolding for task2

---
+++ src/task2/task2.py
@@ -0,0 +1,24 @@
+
+
+class DataManager:
+    """Handles all the logic for pulling data from server"""
+    # TODO: This should be a singleton class 
+    pass
+
+
+class SearchManager:
+    """Handles all operations realated to search"""
+    
+    def search(self):
+        pass
+
+
+class GamingUsersAnalyzer:
+
+    search: SearchManager
+    dataManager: DataManager
+
+    def print_summary():
+        pass
+
+
