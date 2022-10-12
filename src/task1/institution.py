3cb4580d6d91694e08c237d6c27495a2a9c683e3
AhmadTh96 <ahmad.taha.96.96@gmail.com>
2022-10-12 18:20:41 +0400

    Add Time Fucntion

--- src/task1/institution.py
+++ src/task1/institution.py
@@ -12,13 +12,25 @@ class Room(ABC):
         self.activites = activites
 
     def assign_activity(self, time_from, time_to, n_people):
-        if time_from < 8 or time_to > 21:
+        today = calculate_today_stamp()
+        lower_limit = today + 8 * 3600
+        upper_limit = today + 21 * 3600
+        
+        # print(datetime.fromtimestamp(today))
+        # print(datetime.fromtimestamp(time_from))
+        # print(datetime.fromtimestamp(time_to))
+        # print(datetime.fromtimestamp(lower_limit))
+        # print(datetime.fromtimestamp(upper_limit))
+        if time_from < lower_limit or time_from > upper_limit:
             print("You should enter time between 8 to 21")
         elif self.capacity < n_people:
             print(f"Room number {self.number} can't fit for {n_people} people, max capacity is {self.capacity}")
         elif time_from > time_to:
             print("Wrong time format")
+        
         else:
+            time_from = datetime.fromtimestamp(time_from).hour
+            time_to = datetime.fromtimestamp(time_to).hour
             if self.check_available(time_from, time_to):
                 print("Room is not available in this time interval")
             else:
@@ -124,19 +136,30 @@ class LectureAuditorium(Room):
         Room.__init__(self, capacity, number, has_ac, activites)
 
 
+def calculate_today_stamp():
+    return datetime.now().timestamp() - (datetime.now().timestamp() % 86400)
+
+def convert_time(hour_from, minute_from, hour_to, minute_to):
+
+    # datetime.now().timestamp() ==> timestamp of today
+    # datetime.now().timestamp() % 86400 ==> remove days
+    # we do this to eliminate hours to reset to start of the current day
+ 
+    today_stamp = calculate_today_stamp()
+    time_from = today_stamp + (hour_from * 3600) + minute_from * 60
+    time_to = today_stamp + (hour_to * 3600) + minute_to * 60
+
+    return time_from, time_to
+
 
 if __name__ == "__main__":
     # just for testing
     c = Klassroom(19, 1, True)
-    
-    today_stamp = datetime.now().timestamp() - datetime.now().timestamp() % 86400
-    hour_from, minute_from, hour_to, minute_to  = 10, 25, 13, 51
-     
-    time_from = today_stamp + (hour_from * 3600) + minute_from * 60
-    time_to = today_stamp + (hour_to * 3600) + minute_to * 60
-
-    # c.assign_activity(8, 10, 5)
-    # print(c.activites)
+    hour_from, minute_from, hour_to, minute_to = 8, 22, 13, 51
+    time_from, time_to = convert_time(hour_from, minute_from, hour_to, minute_to)
+    c.assign_activity(time_from, time_to, 5)
+    c.assign_activity(time_from, time_to, 5)
+    print(c.activites)
 
     # c.assign_activity(10, 12, 5)
     # c.assign_activity(12, 11, 5)
