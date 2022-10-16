c4578a95861c795a192eb4948f331142839f6664
Vladislav <vladislav.de-gald@yandex.ru>
2022-10-17 02:21:43 +0300

    update ml_model api

--- src/task2/ml_model/api.py
+++ src/task2/ml_model/api.py
@@ -1,14 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+import _pickle as cPickle
+import pandas as pd
 
 app = FastAPI()
 
+with open('classifier.pkl', 'rb') as f:
+    classifier = cPickle.load(f)
 
-# TODO: train regressor, get a prediction with data and return the result
+
 @app.post('/predict')
-async def get_prediction(data: dict[str, float]) -> float:
-    prediction = 35.5
-    return prediction
+async def get_prediction(data: dict) -> list:
+    features_df = pd.DataFrame(data)
+    prediction = classifier.predict(features_df)
+    return [bool(el) for el in prediction]
 
 
 if __name__ == '__main__':
