b16bc3245b9deac0d48124905158f9dd88086b22
Vladislav <vladislav.de-gald@yandex.ru>
2022-10-17 02:22:18 +0300

    minor changes

--- src/task2/main.py
+++ src/task2/main.py
@@ -1,26 +1,5 @@
-from mongo.mongo_client import MongoDBClient
-import requests
-
-MONGO_CLIENT = MongoDBClient('clients')
-ML_MODEL_URL = 'http://ml_model:8080/predict'
-
-
-def start_session() -> None:
-    result = MONGO_CLIENT.db.sessions.find_one({
-        'session_id': 'b3aebc80-ff28-4569-bd18-2ace692f668e',
-    })
-
-    query_params = {
-        'dropped_frames': result['dropped_frames'],
-        'FPS': result['FPS']
-    }
-    print(query_params)
-
-    prediction = requests.post(ML_MODEL_URL, json=query_params)
-    print(f'Prediction: {prediction.text}')
-
-    input('Enter the next command:')
-
+from terminal.terminal import InteractiveTerminal
 
 if __name__ == '__main__':
-    start_session()
+    terminal = InteractiveTerminal()
+    terminal.start_session()
