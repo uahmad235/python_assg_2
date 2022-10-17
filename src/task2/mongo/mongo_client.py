af96e3086bf756a7dfe72cd1ae60728e0f639962
syedjameel <syedjameel9290@gmail.com>
2022-10-17 11:34:18 +0300

    Removed Redundant Files

--- src/task2/mongo/mongo_client.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-
-class MongoDBClient:
-    def __init__(self, db: str) -> None:
-        self.client = MongoClient('mongodb://mongodb:27017')
-        self.db = self.client[db]
